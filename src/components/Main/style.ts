4defa2cd7a44ef445bfa24a87da2c4e0b553b9ab
hdayeon <gkekdus97@naver.com>
2023-12-04 22:49:37 +0900

    initial 푸터

--- src/components/Main/style.ts
+++ src/components/Main/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
     width: 100%;
-    height: 95vh;
+    height: 90vh;
     display: flex;
     flex-direction: column;
     align-items: center;
