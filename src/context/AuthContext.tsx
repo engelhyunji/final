9f05bf63ab00242f3ecd55d8194b114d627a29b6
hdayeon <gkekdus97@naver.com>
2023-12-05 10:02:45 +0900

    로그아웃 시 토큰 삭제

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -10,23 +10,24 @@ interface AuthContextProps {
 const AuthContext = createContext<AuthContextProps | undefined>(undefined)
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [isLogin, setIsLogin] = useState(false)
+    const [isLogin, setIsLogin] = useState(false);
 
     const login = () => {
-        setIsLogin(true)
-    }
+        setIsLogin(true);
+    };
 
     const logout = () => {
-        setIsLogin(false)
-    }
+        setIsLogin(false);
+        localStorage.removeItem('token'); // 인가용 토큰 삭제
+    };
 
     return <AuthContext.Provider value={{ isLogin, login, logout }}>{children}</AuthContext.Provider>
 }
 
 export const useAuth = () => {
-    const context = useContext(AuthContext)
+    const context = useContext(AuthContext);
     if (!context) {
         throw new Error('useAuth는 반드시 AuthProvider내에서 사용되어야 함')
     }
-    return context
-}
+    return context;
+};
