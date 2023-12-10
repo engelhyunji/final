2071874cdbf6a33ac4ffa50c26f29e206e3d233c
hdayeon <gkekdus97@naver.com>
2023-12-10 23:18:41 +0900

    로그인 보수

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -27,11 +27,14 @@ const Login: React.FC = () => {
                 password,
             })
             login() // isLogin 상태변경
-            alert('로그인이 완료되었습니다🐕')
+            alert(`${res.data.nickname}님 로그인이 완료되었습니다🐕`)
             navigate('/')
 
-            const token = res.headers.authorization // 서버 응답의 headers에서 토큰 추출
+            const token = res.headers.authorization // 서버 응답 headers에서 토큰 추출
             localStorage.setItem('accessToken', token)
+
+            console.log('로그인 res.data', res.data);
+            // return res.data;
         } catch (error) {
             console.log('로그인 실패 : error 메세지', error)
         }
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 // 로그인 상태 전역으로 관리
-import React, { createContext, useContext, useState, ReactNode } from 'react'
+import React, { createContext, useContext, useState, ReactNode, useMemo } from 'react'
 
 interface AuthContextProps {
     isLogin: boolean
@@ -7,32 +7,35 @@ interface AuthContextProps {
     logout: () => void
 }
 
-const AuthContext = createContext<AuthContextProps | undefined>(undefined)
+
+export const AuthContext = createContext<AuthContextProps | undefined>(undefined)
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const [isLogin, setIsLogin] = useState(() => {
-        // 페이지 로드 시 토큰 확인
-        const storedToken = localStorage.getItem('accessToken');
-        return !!storedToken; // 토큰 존재하면 true, 아니면 false
-    });
+        const storedToken = localStorage.getItem('accessToken')
+        return !!storedToken // 토큰 존재하면 true, 아니면 false
+    })
+
 
     const login = () => {
         setIsLogin(true);
-    };
+    }
 
     const logout = () => {
-        setIsLogin(false);
-        localStorage.removeItem('accessToken'); // 인가용 토큰 삭제
-        console.log('localStorage.getItem(accessToken) 토큰삭제 확인',localStorage.getItem('accessToken')); // 토큰 삭제 확인
-    };
+        setIsLogin(false)
+        localStorage.removeItem('accessToken') // 인가용 토큰 삭제
+        console.log('localStorage.getItem(accessToken) 토큰삭제 확인', localStorage.getItem('accessToken')) // 토큰 삭제 확인
+    }
 
-    return <AuthContext.Provider value={{ isLogin, login, logout }}>{children}</AuthContext.Provider>
+    const value = useMemo(() => ({ isLogin, login, logout }), [isLogin, login, logout])
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
 
 export const useAuth = () => {
-    const context = useContext(AuthContext);
+    const context = useContext(AuthContext)
     if (!context) {
         throw new Error('useAuth는 반드시 AuthProvider내에서 사용되어야 함')
     }
-    return context;
-};
+    return context
+}
