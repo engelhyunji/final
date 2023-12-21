b7370294ab0fab15dde9696fbc6626bb1613d1fe
hdayeon <gkekdus97@naver.com>
2023-12-21 21:14:59 +0900

    채팅 기본 기능 (디자인 전)

--- src/components/Chat/ChatList.tsx
+++ src/components/Chat/ChatList.tsx
@@ -1,120 +1,114 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect, ChangeEvent, KeyboardEvent } from 'react'
 import * as ST from './style'
 import { useNavigate } from 'react-router-dom'
-import instance from '../../apis/instance'
 import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { addChat, deleteChat, getChatList } from '../../apis/api/chat'
 import { AxiosError } from 'axios'
-import { addChat, deleteChat } from '../../apis/api/Chat'
 
-interface ChatListData {
+interface Chatroom {
     roomId: string
     name: string
-    userCount: number
+    creator: Creator
+}
+export interface Creator {
+    email: string
+    nickname: string
 }
 
 const ChatList: React.FC = () => {
-    const [chatName, setChatName] = useState<ChatListData["name"]>('')
-    const [chatList, setChatList] = useState<ChatListData[]>([])
-
     const navigate = useNavigate()
-    const enterChat = (roomId:ChatListData["roomId"]) => {
-        navigate(`/chat/${roomId}`)
-    }
+    const queryClient = useQueryClient()
+    const [roomName, setRoomName] = useState<string>('')
+    const [chatrooms, setChatrooms] = useState<Chatroom[]>([])
 
-    const myChatttingList = async () => {
-        try {
-            const res = await instance.get('/chat/rooms')
-            return res.data.data.chatRoomResponseDtos
-        } catch (err) {
-            console.log(err)
+    const { data, isSuccess } = useQuery('getChatList', getChatList)
+    useEffect(() => {
+        if (isSuccess && data) {
+            setChatrooms(data)
+            console.log('채팅방 목록 조회 성공 data : ', data)
         }
-    }
+    }, [isSuccess, data])
 
-    useQuery('getChatList', myChatttingList, {
-        onSuccess: (data) => {
-            if (data) {
-                setChatList(data)
-                // console.error('data 있으면 chatList', chatList)
-            } else {
-                // 반환된 데이터가 배열이 아니거나 null/undefined
-                console.error('data 받기 실패 시 data', data)
-            }
+    const addChatMutation = useMutation<void, AxiosError, { name: string }>(({ name }) => addChat({name}), {
+        onSuccess: () => {
+            setRoomName('')
+            queryClient.invalidateQueries('getChatList')
         },
-        onError: (error) => {
-            console.error('getChatList 에러 :', error)
+        onError: () => {
+            alert('채팅방 생성에 실패했습니다.')
         },
     })
 
-    useEffect(() => {
-        console.log('렌더링 시 chatList', chatList)
-    }, [chatList])
-
-    const queryClient = useQueryClient();
-    const addMutation = useMutation<void, AxiosError, { chatName: string }>(
-        ({ chatName }) => addChat(chatName),
-        {
-            onSuccess: () => {
-                queryClient.invalidateQueries('getChatList')
-                setChatName('')
-            },
-            onError: (error) => {
-                console.error('채팅방추가 Mutation 에러 :', error)
-                alert('채팅 생성에 실패했습니다.')
-            },
+    const deleteChatMutation = useMutation<void, AxiosError, { roomId: string }>(({ roomId }) => deleteChat(roomId), {
+        onSuccess: () => {
+            queryClient.invalidateQueries('getChatList')
         },
-    )
+    })
 
-    const deleteMutation = useMutation<void, AxiosError, { roomId: string }>(
-        ({ roomId }) => deleteChat(roomId),
-        {
-            onSuccess: () => {
-                queryClient.invalidateQueries('getChatList')
-            },
-        },
-    )
-
-    const addChatHandler = async () => {
-        addMutation.mutate({ chatName })
+    const createRoom = (): void => {
+        if (roomName === '') {
+            alert('방 제목을 입력해 주세요.')
+            return
+        } else {
+            const params = new URLSearchParams()
+            params.append('name', roomName)
+            addChatMutation.mutate({ name: roomName })
+        }
     }
 
-    if (chatList?.length === 0) {
-        return (
-            <ST.EmptyWrap>
-                <ST.EmptyText>진행 중인 채팅이 없습니다.</ST.EmptyText>
-            </ST.EmptyWrap>
-        )
+    const DeleteHandler = (roomId: string) => {
+        if (confirm('채팅방을 삭제하시겠습니까?')) {
+            deleteChatMutation.mutate({ roomId })
+        }
+    }
+
+    const enterRoom = (roomId: string): void => {
+        if (confirm('채팅방에 입장하시겠습니까?')) {
+            navigate(`/chat/room/enter/${roomId}`)
+        }
     }
 
     return (
-        <ST.ChatContainer>
+        <ST.ChatContainer id="app">
             <div>
                 <ST.ChatH2>채팅방 만들기</ST.ChatH2>
-                <ST.ChatLabel>채팅방 이름: </ST.ChatLabel>
-                <ST.ChatNameInput value={chatName} onChange={(e) => setChatName(e.target.value)}/>
-                <ST.ChatBtn onClick={addChatHandler}>채팅방 생성</ST.ChatBtn>
             </div>
-            <div>
-                <ST.ChatH2>채팅</ST.ChatH2>
-                <ST.ChatLists>
-                    {chatList.map((item, index) => {
-                        return (
-                            <ST.ChatList key={index} onClick={() => enterChat(item.roomId)}>
-                                <div>
-                                    roomId: {item.roomId}
-                                    <div>
-                                        <p>
-                                            방제목: {item.name}
-                                            {/* 제목 */}
-                                        </p>
-                                        <p>참여자수: {item.userCount}</p>
-                                        <ST.ChatBtn onClick={() => deleteMutation.mutate({ roomId: item.roomId })}>방 삭제하기</ST.ChatBtn>
-                                    </div>
-                                </div>
+
+            <ST.ChatInputDiv>
+                <div>
+                    <ST.ChatLabel>채팅방 이름: </ST.ChatLabel>
+                </div>
+                <ST.ChatNameInput
+                    type="text"
+                    value={roomName}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setRoomName(e.target.value)}
+                    onKeyUp={(e: KeyboardEvent<HTMLInputElement>) => e.key === 'Enter' && createRoom()}
+                />
+                <div>
+                    <ST.ChatBtn type="button" onClick={createRoom}>
+                        채팅방 개설
+                    </ST.ChatBtn>
+                </div>
+            </ST.ChatInputDiv>
+            <ST.ChatH2>채팅방 목록</ST.ChatH2>
+            <ST.ChatLists>
+                {chatrooms.length === 0 ? (
+                    <ST.EmptyWrap>
+                        <ST.EmptyText>진행 중인 채팅이 없습니다.</ST.EmptyText>
+                    </ST.EmptyWrap>
+                ) : (
+                    chatrooms.map((item) => (
+                        <div key={item.roomId}>
+                            <ST.ChatList onClick={() => enterRoom(item.roomId)}>
+                                {/* 방 ID: {item.roomId} <br /> */}
+                                방 이름: {item.name} <br />
+                                방장 : {item.creator.nickname}
                             </ST.ChatList>
-                        )
-                    })}
-                </ST.ChatLists>
-            </div>
+                            <button onClick={() => DeleteHandler(item.roomId)}>삭제</button>
+                        </div>
+                    ))
+                )}
+            </ST.ChatLists>
         </ST.ChatContainer>
     )
 }
