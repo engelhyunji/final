5c4055b299a5346e180f137f998e5da1df643c4e
hdayeon <gkekdus97@naver.com>
2023-12-27 00:33:28 +0900

    Shop 상세페이지 리뷰 스타일파일

---
+++ src/components/Shops/Reviews/style.ts
@@ -0,0 +1,95 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100%;
+    /* padding: 85px 0; */
+`
+export const ReviewInputP = styled.p`
+    width: 100%;
+    font-family: Pretendard-regular;
+    margin: 50px 0;
+
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    justify-content: center;
+`
+export const ReviewInput = styled.input`
+    width: 407px;
+    padding: 11px 13px;
+
+    border-radius: 7px;
+    border: 1px solid #dadada;
+    background: #fff;
+`
+export const AddBtn = styled.button`
+    padding: 15px;
+    color: #fff;
+    font-family: Pretendard-medium;
+    background-color: #00bd8f;
+    border: none;
+    border-radius: 8px;
+`
+
+export const ReviewH3 = styled.h3`
+    margin: 26px 0;
+    font-family: Pretendard-bold;
+    font-size: 22px;
+    line-height: 28px;
+`
+
+export const ReviewListUl = styled.ul`
+    width: 100%;
+
+    display: flex;
+    justify-content: space-between;
+`
+
+export const ReviewListLi = styled.li`
+    width: 390px;
+    padding: 24px 36px;
+
+    font-size: 20px;
+    font-family: Pretendard-regular;
+
+    border-radius: 10px;
+    border: 0.5px solid #dadada;
+    background: #fff;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 16px;
+`
+
+export const ReviewListP = styled.p`
+    width: 100%;
+
+    display: flex;
+    justify-content: space-between;
+`
+
+export const ReviewNick = styled.span`
+    font-size: 14px;
+    font-family: Pretendard-regular;
+    color: #8F8E93;
+`
+export const GoodBtn = styled.button`
+    padding: 6px 8px 8px;
+    font-size: 20px;
+    /* color: #fff; */
+    /* background-color: #00bd8f; */
+    border: none;
+    border-radius: 8px;
+`
+
+export const DelBtn = styled.button`
+    width: 50px;
+    height: 34px;
+    color: #FD4141;
+    font-size: 14px;
+    font-family: Pretendard-Regular;
+    background-color: #eee;
+    border: none;
+    border-radius: 8px;
+`
