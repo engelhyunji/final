f929b74d16731c110cc48c1a52f01bb5fdceb91b
engelhyunji <hhzy1457@nate.com>
2023-12-28 01:36:33 +0900

    지도

--- src/components/Map/MapComponent.tsx
+++ src/components/Map/MapComponent.tsx
@@ -77,25 +77,49 @@ const MapComponent: React.FC<MapComponentProps> = ({ coords }) => {
         }
     }
 
+    // const saveSearchResults = async () => {
+    //     try {
+    //         // Axios 인스턴스를 사용하여 결과를 서버로 보내는 API 호출
+    //         const response = await instance.post('/api/map', { places })
+
+    //         if (response.status === 200) {
+    //             // 성공적으로 저장된 경우
+    //             console.log('검색 결과가 성공적으로 저장되었습니다.')
+    //         } else {
+    //             // 저장 실패 또는 오류 발생한 경우
+    //             console.error('검색 결과 저장 실패:', response.statusText)
+    //             throw new Error('검색 결과 저장 실패')
+    //         }
+    //     } catch (error) {
+    //         // 예외 발생 시 처리
+    //         console.error('검색 결과 저장 에러:', error)
+    //         throw error
+    //     }
+    // }
+
+    // 검색 결과를 백엔드에 저장하는 함수
     const saveSearchResults = async () => {
         try {
-            // Axios 인스턴스를 사용하여 결과를 서버로 보내는 API 호출
-            const response = await instance.post('/api/map', { places })
+            const transformedPlaces = places.map(place => ({
+                address: place.address_name,
+                latitude: parseFloat(place.y),
+                longitude: parseFloat(place.x)
+            }));
+
+            const response = await instance.post('/api/map', transformedPlaces);
 
             if (response.status === 200) {
-                // 성공적으로 저장된 경우
-                console.log('검색 결과가 성공적으로 저장되었습니다.')
+                console.log('검색 결과가 성공적으로 저장되었습니다.');
             } else {
-                // 저장 실패 또는 오류 발생한 경우
-                console.error('검색 결과 저장 실패:', response.statusText)
-                throw new Error('검색 결과 저장 실패')
+                console.error('검색 결과 저장 실패:', response.statusText);
+                throw new Error('검색 결과 저장 실패');
             }
         } catch (error) {
-            // 예외 발생 시 처리
-            console.error('검색 결과 저장 에러:', error)
-            throw error
+            console.error('검색 결과 저장 에러:', error);
+            throw error;
         }
-    }
+    };
+
 
     // 저장된 검색 결과를 불러오는 함수
     const loadSavedResults = async () => {
