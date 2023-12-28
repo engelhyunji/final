fd343c84d7885d3999ec648f25c3dd269a2ed0c8
hdayeon <gkekdus97@naver.com>
2023-12-28 14:42:54 +0900

    사용안하는 res 값 제거

--- src/apis/api/chat.ts
+++ src/apis/api/chat.ts
@@ -13,7 +13,7 @@ export const getChatList = async () => {
 // 채팅방 생성
 export const addChat = async (query: { name: string }) => {
     try {
-        const res = await instance.post('/chat/room', null, { params: query })
+        await instance.post('/chat/room', null, { params: query })
         // console.log('addChat 함수 반환값',res.data.result)
     } catch (err) {
         console.log(err)
