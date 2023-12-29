fe33f51ece150b9f793b8186afaa532f1cfdb902
engelhyunji <hhzy1457@nate.com>
2023-12-29 15:57:04 +0900

    펫

--- src/components/Pet/PetDetail/PetDetail.tsx
+++ src/components/Pet/PetDetail/PetDetail.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import * as ST from './style'
 import { PetDetails, fetchPetDetail } from '../../../apis/api/pet'
-import NoLineLink from '../../NoLineLink'
+// import NoLineLink from '../../NoLineLink'
 
 type PetDetailParams = {
     [key: string]: string | undefined // petId가 undefined일 수 있음을 명시
@@ -57,11 +57,11 @@ const PetDetail: React.FC = () => {
                     <ST.DetailLabel>Info: </ST.DetailLabel>
                     {pet.petInfo}
                 </ST.DetailText>
-                <ST.Wrap1>
+                {/* <ST.Wrap1>
                     <NoLineLink to={`/my`}>
                         <ST.Text1>마이 페이지로 이동 ⇀</ST.Text1>
                     </NoLineLink>
-                </ST.Wrap1>
+                </ST.Wrap1> */}
             </ST.DetailCard>
             <ST.Posts>
                 {pet.imageUrls.map((url, index) => (
