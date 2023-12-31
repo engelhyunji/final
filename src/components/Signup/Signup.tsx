b52dbc6cbde7628066c21b176f3e85f7049d147a
hdayeon <gkekdus97@naver.com>
2024-01-01 01:20:31 +0900

    인증코드 발송 시 마다 타이머 재시작하도록 키 설정

--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -16,6 +16,9 @@ const Signup: React.FC = () => {
     const [code, setCode] = useState('')
     // 인증코드 5분 타이머 컨트롤
     const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false)
+    // 타이머를 강제로 다시 렌더링하기 위한 키
+    const [timerKey, setTimerKey] = useState<number>(0); 
+
     const [userData, setUserData] = useState<UserData>({
         nickname: '',
         phoneNumber: '',
@@ -38,6 +41,8 @@ const Signup: React.FC = () => {
                 await instance.post('/api/user/email', {email})
                 alert('이메일로 인증코드가 발송되었습니다.')
                 setIsTimerRunning(true)
+                // 키 변경해서 타이머 재시작
+                setTimerKey((prevKey) => prevKey + 1); 
             } catch (err) {
                 console.log('이메일 전송에러 :', err)
                 alert('이미 가입된 이메일 입니다.')
@@ -134,7 +139,7 @@ const Signup: React.FC = () => {
                                 value={code}
                                 onChange={(e) => setCode(e.target.value)}
                             />
-                            {isTimerRunning && <Timer mm={'5'} ss={'0'} isRunning={isTimerRunning} />}
+                            {isTimerRunning && <Timer key={timerKey} mm={'5'} ss={'0'} isRunning={isTimerRunning} />}
                         </ST.SignupInputDiv>
                         <ST.ComfirmBtn onClick={()=>codeVerify(userData.email, code)}>확인</ST.ComfirmBtn>
                     </ST.VerifyBox>
