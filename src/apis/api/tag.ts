fc9da232cd5a8178ccabf873f6038b61ee3b3920
hdayeon <gkekdus97@naver.com>
2024-01-01 11:49:23 +0900

    해시태그 관련 api호출 코드 모음 정리

---
+++ src/apis/api/tag.ts
@@ -0,0 +1,82 @@
+import instance from "../instance"
+
+// 채팅방 해시태그 방별 추가
+export const AddHash = async (roomId: string, hash: string) => {
+    try {
+        await instance.post(`${import.meta.env.VITE_APP_SERVER_URL}/api/tags/chatrooms/${roomId}`, [hash] )
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// 채팅방 해시태그 인기 조회
+export const getPopularHash = async () => {
+    try {
+        const res = await instance.get('api/tags/chatrooms/popular')
+        return res.data.result
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// 채팅방 해시태그 별 방조회
+export const getHashRoomList = async (tagName: string) => {
+    try {
+        const res = await instance.get(`api/tags/chatrooms/${tagName}`)
+        return res.data.result
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// 채팅방 해시태그 삭제
+export const deleteHash = async (roomId : string, tagName: string) => {
+    try {
+        const res = await instance.delete(`api/tags/chatrooms/${roomId}/${tagName}`)
+        return res.data.result
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+
+
+// Shop 해시태그 방별 추가
+export const AddShopHash = async (shopId: string, hash: string) => {
+    try {
+        await instance.post(`${import.meta.env.VITE_APP_SERVER_URL}/api/tags/shops/${shopId}`, [hash] )
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// Shop 해시태그 인기 조회
+export const getShopPopularHash = async () => {
+    try {
+        const res = await instance.get('api/tags/shops/popular')
+        return res.data.result
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// Shop 해시태그 별 방조회
+export const getShopHashRoomList = async (tagName: string) => {
+    try {
+        const res = await instance.get(`api/tags/shops/${tagName}`)
+        return res.data.result
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// Shop 해시태그 삭제
+export const deleteShopHash = async (shopId : string, tagName: string) => {
+    try {
+        const res = await instance.delete(`api/tags/shops/${shopId}/${tagName}`)
+        return res.data.result
+    } catch (err) {
+        console.log(err)
+    }
+}
