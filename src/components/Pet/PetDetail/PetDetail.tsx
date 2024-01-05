9db346044554cc037ad86ba5f03b434991d6b9e7
engelhyunji <hhzy1457@nate.com>
2024-01-06 03:41:02 +0900

    최종 재배포 수정

--- src/components/Pet/PetDetail/PetDetail.tsx
+++ src/components/Pet/PetDetail/PetDetail.tsx
@@ -1,9 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useParams } from 'react-router-dom'
-import { fetchPetDetail, PetDetails } from '../../../apis/api/petlist'
+import { fetchPetDetail } from '../../../apis/api/petlist'
 import * as ST from './style'
 import Modal from 'react-modal'
+
+export interface PetDetails {
+    userId: number
+    petId: number
+    nickname: string
+    petName: string
+    petGender: 'MALE' | 'FEMALE'
+    petKind: 'SMALL' | 'MEDIUM' | 'LARGE'
+    petInfo: string
+    imageUrls: string[]
+    petLikes: number // 좋아요 수
+}
+
+// ApiResponse 타입 정의
+export interface ApiResponse<T> {
+    isSuccess: boolean;
+    code: number;
+    message: string;
+    result: T;
+}
+
 
 Modal.setAppElement('#root')
 
