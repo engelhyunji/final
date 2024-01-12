cdc6e6da54dba94ef4876624efd667e0d4957d00
engelhyunji <hhzy1457@nate.com>
2024-01-12 18:22:34 +0900

    리덕스코드제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'; // react-redux의 Provider를 import
 import AppRoutes from './router/AppRoutes';
 import './App.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { StyleSheetManager } from 'styled-components';
 import isPropValid from '@emotion/is-prop-valid';
-import store from './redux/config/configStore';
-// import MapComponent from './components/Map/MapComponent';
 
 const queryClient = new QueryClient();
 
 const App = () => {
-    // const coords = {
-    //     lat: 33.450701, // 기본 위도
-    //     lng: 126.570667 // 'lng'로 변경
-    // };
-
     return (
         <StyleSheetManager shouldForwardProp={(prop) => isPropValid(prop) && prop !== 'isSelected'}>
             <QueryClientProvider client={queryClient}>
-                <Provider store={store}>
-                    <AppRoutes />
-                </Provider>
-                {/* <MapComponent coords={coords} /> */}
+                <AppRoutes />
             </QueryClientProvider>
         </StyleSheetManager>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-// import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import store from './redux/config/configStore'
-import { Provider } from 'react-redux'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(
         <App />
-    </Provider>,
-)
+    );
+} else {
+    console.error('Root element not found');
+}
